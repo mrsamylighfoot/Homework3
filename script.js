e8d2e95f4d81cb9687d470de90c82c6e85066277
Amy Lightfoot <MrsAmyLightfoot@gmail.com>
2022-03-25 20:15:21 -0400

    Attempted to update create password

--- script.js
+++ script.js
@@ -7,32 +7,29 @@ var lC = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 // numbers
 var num = String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 // special characters
-// var special = 
+var special = ['!', '@','#','$','%','^','&','*','(',')','_','+','-','=']
+
 
 function generatePassword(){
   console.log("Hey! You clicked the button");
 
   // 1. Prompt user for the password criteria: 
     //  a. length between 8 and 128
-var numberCharacters = window.prompt("How many characters do you want? (Enter number between 8 and 128");
+  var numberCharacters = window.prompt("How many characters do you want? (Enter number between 8 and 128");
 
 if (numberCharacters <8 || numberCharacters >128){
   window.alert("Number needs to be between 8 and 128")
 }
-
-console.log(numberCharacters);
   //  b. lowercase, uppercase, numbers, special characters
   var upperCase = window.confirm("Click OK to include Upper Case.");
-console.log(upperCase);
-var lowerCase = window.confirm("Click OK to include lower case.");
-console.log(lowerCase);
-var numbers = window.confirm("Click OK to include numbers.");
-console.log(numbers);
-var specialCharacters = window.confirm("Click OK to include special characters.");
-console.log(specialCharacters);
+  var lowerCase = window.confirm("Click OK to include lower case.");
+  var numbers = window.confirm("Click OK to include numbers.");
+  var specialCharacters = window.confirm("Click OK to include special characters.");    
   // 2. Validate user input
   // 3. Generate password based on criteria
-
+for (i=0; i<numberCharacters; i++){
+  
+}
   // 4. Display password on page
   return newPassword;
 }
