a8c4c2f5a9d6114942d52b8137f8b046ba60bee8
Amy Lightfoot <MrsAmyLightfoot@gmail.com>
2022-03-31 16:52:32 -0400

    Updated functions

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // A-Z
-var uC = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+var uC = ['A', 'B', 'C']
+console.log("UC: ", uC)
 // a-z
-var lC = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+var lC = ['a', 'b', 'c']
 // numbers
-var num = String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+var num = ['1', '2', '3']
 // special characters
 var special = ['!', '@','#','$','%','^','&','*','(',')','_','+','-','=']
 
@@ -17,22 +18,52 @@ function generatePassword(){
     //  a. length between 8 and 128
   var numberCharacters = window.prompt("How many characters do you want? (Enter number between 8 and 128");
 
-if (numberCharacters <8 || numberCharacters >128){
-  window.alert("Number needs to be between 8 and 128")
-}
+  if (numberCharacters <8 || numberCharacters >128){
+    window.alert("Number needs to be between 8 and 128");
+    return;
+  }
   //  b. lowercase, uppercase, numbers, special characters
   var upperCase = window.confirm("Click OK to include Upper Case.");
   var lowerCase = window.confirm("Click OK to include lower case.");
   var numbers = window.confirm("Click OK to include numbers.");
   var specialCharacters = window.confirm("Click OK to include special characters.");    
   // 2. Validate user input
+  if (upperCase === false && lowerCase === false && numbers === false && specialCharacters === false){
+    window.alert("Please choose at least one character type.")
+    return;
+  }
   // 3. Generate password based on criteria
-for (i=0; i<numberCharacters; i++){
-  
+  var combinedChar = [];
+  if (upperCase === true){
+    combinedChar=combinedChar.concat(uC)
+  };
+  if (lowerCase === true){
+    combinedChar=combinedChar.concat(lC)
+  };
+  if (numbers === true){
+    combinedChar=combinedChar.concat(num)
+  };
+  if (specialCharacters === true){
+    combinedChar=combinedChar.concat(special)
+  };
+console.log("Combine Char Array: ", combinedChar)
+
+var newPassword = []
+for (i=0; i < numberCharacters; i++){
+  var character = randomIze(combinedChar)
+ newPassword.push(character)
 }
   // 4. Display password on page
-  return newPassword;
+  return newPassword.join('');
 }
+
+function randomIze (array){
+  var index = Math.floor(Math.random()*array.length)
+  var character = array[index];
+  return character
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -45,3 +76,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+var pword = document.querySelector("#password");
+$('#password')
+
+pword.textContent = "Hello"
+$('#password').text("Hello")
+
+
+// Not JQuery
+var $variable
