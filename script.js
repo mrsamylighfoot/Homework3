10a051f93f23456c8b01dcbc41642cba02b86c12
Amy Lightfoot <MrsAmyLightfoot@gmail.com>
2022-04-01 10:57:29 -0400

    Updated letters and numbers to include full array

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // A-Z
-var uC = ['A', 'B', 'C']
+var uC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
 console.log("UC: ", uC)
 // a-z
-var lC = ['a', 'b', 'c']
+var lC = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 // numbers
-var num = ['1', '2', '3']
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 // special characters
 var special = ['!', '@','#','$','%','^','&','*','(',')','_','+','-','=']
 
